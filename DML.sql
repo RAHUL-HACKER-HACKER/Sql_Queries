0bdf69047e34566c1656509e76bc0509874d2fcf
RAHUL KUMAR <86794622+RAHUL-HACKER-HACKER@users.noreply.github.com>
2023-06-05 00:55:46 +0530

    second commit

---
+++ DML.sql
@@ -0,0 +1,10 @@
+INSERT INTO Employee (Author, Subject) VALUES ("Sonoo", "DBMS");  
+
+UPDATE students    
+SET User_Name = 'Sonoo'    
+WHERE Student_Id = '3' ;
+
+DELETE FROM employee  
+WHERE Author="Sonoo"; 
+
+ 
